96f3d0e5e907f63ba0a98aaaa1337fb9c2702500
warangkana <warangkana_chit@cmu.ac.th>
2024-07-22 17:27:01 +0700

    code 1

--- src/app/page.js
+++ src/app/page.js
@@ -1,51 +1,39 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TaskInput from "./components/TaskInput";
+import Task from "./components/Task";
+
 export default function Home() {
+
+  const Tasks = [
+  {id: 1, title: "Read a book", isDone: true},
+  {id: 2, title: "Take a shower", isDone: true},
+  {id: 3, title: "Sleep", isDone: true}
+  ]
+
   return (
     // Main container
     <div className="container mx-auto">
+      
       {/* header section */}
-      <div>
-        <h4 className="display-4 fst-italic text-center">Todo List Mock Up</h4>
-        <p className="text-secondary text-center">
-          This lab teach you how to split react component(s)
-        </p>
-      </div>
+      <Header />
 
       {/* tasks container */}
       <div style={{ maxWidth: "400px" }} className="mx-auto">
+      
         {/* task input */}
-        <div className="d-flex gap-1">
-          <input
-            className="form-control"
-            placeholder="Insert a task here.."
-          ></input>
-          <button className="btn btn-primary">Add</button>
-        </div>
+        <TaskInput />
+
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Read a book</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task {...Tasks[0]} />
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Take a shower</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task {...Tasks[1]} />
         {/* task */}
-        <div className="d-flex p-3 gap-2 align-items-center border-bottom">
-          <span>Sleep</span>
-          <button className="btn btn-success">Done</button>
-          <button className="btn btn-danger">Delete</button>
-        </div>
+        <Task {...Tasks[2]} />
       </div>
 
       {/* //footer section */}
-      <div className="mt-3">
-        <p className="text-secondary text-center">
-          copyright © 2023 Chayanin Suatap 12345678
-        </p>
-      </div>
+      <Footer name="Warangkana Jitwarangkana" code="660610792"/>
     </div>
   );
 }
